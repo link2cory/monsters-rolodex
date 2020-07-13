c8da2d5c81192b7d816c3d1e267d6fe709e666ea
Cory Perkins <link2cory@gmail.com>
2020-07-13 14:57:51 -0700

    Updates

---
+++ precache-manifest.a00e6f9749a50fb9a376c7c998854fd6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59257f1d9b4e5b2678be00402f7b27f6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "26eaffd2ab2ff9ccccbf",
+    "url": "/monsters-rolodex/static/css/main.09c089c4.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26eaffd2ab2ff9ccccbf",
+    "url": "/monsters-rolodex/static/js/main.3c47b439.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
